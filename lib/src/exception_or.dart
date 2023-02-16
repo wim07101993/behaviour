f2ade82bd4333c675ebe3c7c393700c44f4549b9
Wim Van Laer <wim07101993@users.noreply.github.com>
2023-02-16 22:33:56 +0100

    feat: added whenSuccess and whenFailed extensions for Future<ExceptionOr<T>>

--- lib/src/exception_or.dart
+++ lib/src/exception_or.dart
@@ -33,12 +33,9 @@ abstract class ExceptionOr<TSuccess> {
 
   /// Executes [next] only if the current implementation is [Success].
   ExceptionOr<TSecondSuccess> startNextWhenSuccess<TSecondSuccess>(
-    ExceptionOr<TSecondSuccess> Function() next,
+    ExceptionOr<TSecondSuccess> Function(TSuccess value) next,
   ) {
-    return when(
-      (exception) => Failed(exception),
-      (value) => next(),
-    );
+    return when((exception) => Failed(exception), next);
   }
 }
 
@@ -112,13 +109,24 @@ extension FutureExceptionOrExtensions<T> on Future<ExceptionOr<T>> {
     return then((value) => value.when(ifFailed, ifSuccess));
   }
 
+  /// Executes [ifSuccess] if the future results in a[Success].
+  Future<ExceptionOr<T>> thenWhenSuccess<TResult>(
+    FutureOr<TResult> Function(T value) ifSuccess,
+  ) {
+    return then((value) => value.whenSuccess(ifSuccess));
+  }
+
+  /// Executes [ifFailed] if the future results in a [Failed].
+  Future<ExceptionOr<T>> thenWhenFailed<TResult>(
+    FutureOr<TResult> Function(Exception exception) ifFailed,
+  ) {
+    return then((value) => value.whenFailed(ifFailed));
+  }
+
   /// Executes [next] only if the current future results in a [Success].
   Future<ExceptionOr<TSecondSuccess>> thenStartNextWhenSuccess<TSecondSuccess>(
     Future<ExceptionOr<TSecondSuccess>> Function(T value) next,
   ) {
-    return thenWhen(
-      (exception) => Failed(exception),
-      (value) => next(value),
-    );
+    return thenWhen((exception) => Failed(exception), next);
   }
 }
--- test/src/exception_or_test.dart
+++ test/src/exception_or_test.dart
@@ -1,35 +1,107 @@
 import 'package:behaviour/behaviour.dart';
 import 'package:faker/faker.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
+abstract class _FunctionWrapper<TInput> {
+  void call(TInput input);
+}
+
+class MockFunction<T> extends Mock implements _FunctionWrapper<T> {}
+
+class MockExceptionOr<T> extends Mock implements ExceptionOr<T> {}
+
 void main() {
+  setUpAll(() {
+    registerFallbackValue(Exception());
+  });
+
   group(ExceptionOr, () {
     group('startNextWhenSuccess', () {
-      test('should return first failure if not success', () async {
-        // arrange
-        final failed = Failed(Exception(faker.lorem.sentence()));
-
-        // act
-        final result = failed.startNextWhenSuccess(() {
-          return Failed(Exception(faker.lorem.word()));
-        });
-
-        // assert
-        expect(result, failed);
-      });
-
-      test('should invoke next if current results to success', () async {
+      test('should invoke next if current results to success', () {
         // arrange
         final success = Success(faker.lorem.sentence());
         final secondSuccess = Success(faker.lorem.word());
 
         // act
-        final result = success.startNextWhenSuccess(() {
+        final result = success.startNextWhenSuccess((value) {
+          expect(value, success.value);
           return secondSuccess;
         });
 
         // assert
         expect(result, secondSuccess);
+      });
+
+      test('should return first failure if not success', () {
+        // arrange
+        final failed = Failed(Exception(faker.lorem.sentence()));
+
+        // act
+        final result = failed.startNextWhenSuccess((value) {
+          throw AssertionError(
+            'Failed is no success. This action should not have been called',
+          );
+        });
+
+        // assert
+        expect(result, failed);
+      });
+    });
+
+    group('whenSuccess', () {
+      test('should execute ifSuccess when the exceptionOr is a success', () {
+        // arrange
+        final fakeValue = faker.lorem.sentence();
+        final function = MockFunction<String>();
+        final ExceptionOr<String> exceptionOr = Success(fakeValue);
+
+        // act
+        exceptionOr.whenSuccess(function);
+
+        // assert
+        verify(() => function.call(fakeValue));
+      });
+
+      test('should not execute ifSuccess when the exceptionOr is a failed', () {
+        // arrange
+        final function = MockFunction<String>();
+        final ExceptionOr<String> exceptionOr = Failed(
+          Exception(faker.lorem.sentence()),
+        );
+
+        // act
+        exceptionOr.whenSuccess(function);
+
+        // assert
+        verifyNever(() => function.call(any()));
+      });
+    });
+
+    group('whenFailed', () {
+      test('should execute ifFailed when the exceptionOr is a failed', () {
+        // arrange
+        final fakeException = Exception(faker.lorem.sentence());
+        final function = MockFunction<Exception>();
+        final ExceptionOr<String> exceptionOr = Failed(fakeException);
+
+        // act
+        exceptionOr.whenFailed(function);
+
+        // assert
+        verify(() => function.call(fakeException));
+      });
+
+      test('should not execute ifFailed when the exceptionOr is a success', () {
+        // arrange
+        final function = MockFunction<Exception>();
+        final ExceptionOr<String> exceptionOr = Success(faker.lorem.sentence());
+
+        // act
+        exceptionOr.whenFailed(function);
+
+        // assert
+        verifyNever(() => function.call(any()));
       });
     });
   });
@@ -152,29 +224,85 @@ void main() {
   });
 
   group('FutureExceptionOrExtensions', () {
-    group('thenWhen', () {
-      test('should invoke the ifException if failed', () async {
-        // act
-        final exception = Exception(faker.lorem.sentence());
-        final failed = Future.value(Failed(exception));
+    late Exception fakeException;
+    late ExceptionOr<String> mockExceptionOr;
 
-        // assert
-        await failed.thenWhen(
-          (e) => expect(e, exception),
+    setUp(() {
+      fakeException = Exception(faker.lorem.sentence());
+      mockExceptionOr = MockExceptionOr();
+
+      when(() => mockExceptionOr.whenFailed(any())).thenReturn(mockExceptionOr);
+      when(() => mockExceptionOr.whenSuccess(any()))
+          .thenReturn(mockExceptionOr);
+      when(() => mockExceptionOr.when(any(), any())).thenAnswer((i) {
+        // ignore: avoid_dynamic_calls
+        return i.positionalArguments[0](fakeException);
+      });
+    });
+
+    group('thenWhen', () {
+      test('should invoke the ifException when failed', () async {
+        // arrange
+        final exception = Exception(faker.lorem.sentence());
+        final failed = Failed(exception);
+        final future = Future.value(failed);
+        final fakeReturnValue = faker.lorem.sentence();
+
+        // act
+        final returnValue = await future.thenWhen(
+          (e) => fakeReturnValue,
           (value) => throw AssertionError('Should have invoked success.'),
         );
-      });
-
-      test('should invoke the ifSuccess', () async {
-        // act
-        final value = faker.lorem.sentence();
-        final success = Future.value(Success(value));
 
         // assert
-        await success.thenWhen(
+        expect(returnValue, fakeReturnValue);
+      });
+
+      test('should invoke the ifSuccess when success', () async {
+        // act
+        final value = faker.lorem.sentence();
+        final success = Success(value);
+        final future = Future.value(success);
+        final fakeReturnValue = faker.lorem.sentence();
+
+        // act
+        final returnValue = await future.thenWhen(
           (exception) => throw AssertionError('Should have invoked exception.'),
-          (v) => expect(v, value),
+          (v) => fakeReturnValue,
         );
+
+        // assert
+        expect(returnValue, fakeReturnValue);
+      });
+    });
+
+    group('thenWhenSuccess', () {
+      test('should invoke the whenSuccess of the exceptionOr', () async {
+        // arrange
+        final future = Future.value(mockExceptionOr);
+        String successHandler(String value) => faker.lorem.sentence();
+
+        // act
+        final returnValue = await future.thenWhenSuccess(successHandler);
+
+        // assert
+        expect(returnValue, mockExceptionOr);
+        verify(() => mockExceptionOr.whenSuccess(successHandler));
+      });
+    });
+
+    group('thenWhenFailed', () {
+      test('should invoke the whenFailed of the exceptionOr', () async {
+        // arrange
+        final future = Future.value(mockExceptionOr);
+        String exceptionHandler(Exception value) => faker.lorem.sentence();
+
+        // act
+        final returnValue = await future.thenWhenFailed(exceptionHandler);
+
+        // assert
+        expect(returnValue, mockExceptionOr);
+        verify(() => mockExceptionOr.whenFailed(exceptionHandler));
       });
     });
 
